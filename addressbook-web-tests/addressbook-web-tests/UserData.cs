14d600d9a15d7db0655ec2ff344bd8f0ada2c0c5
AWise <AWise@AWISE>
2018-08-26 20:21:50 +0300

    UserCreationTests

---
+++ addressbook-web-tests/addressbook-web-tests/UserData.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressBookTests
+{
+    class UserData
+    {
+        private string fFirstname;
+        private string fLastname = "";
+        private string fAddress = "";
+        private string fTelephone = "";
+        private string fEmail = "";
+
+        // конструктор
+        public UserData(string firstname)
+        {
+            fFirstname = firstname;
+        }
+
+        public string FirstName
+        {
+            get { return fFirstname; }
+            set { fFirstname = value; }
+        }
+
+        public string LastName
+        {
+            get { return fLastname; }
+            set { fLastname = value; }
+        }
+
+        public string Address
+        {
+            get { return fAddress; }
+            set { fAddress = value; }
+        }
+
+        public string Telephone
+        {
+            get { return fTelephone; }
+            set { fTelephone = value; }
+        }
+
+        public string Email
+        {
+            get { return fEmail; }
+            set { fEmail = value; }
+        }
+    }
+}
