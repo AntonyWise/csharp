4809da44575c72dae95acbdf42632e986457fb89
AWise <AWise@AWISE>
2018-09-15 11:29:25 +0300

    Rebuild User Tests

--- addressbook-web-tests/addressbook-web-tests/tests/UserCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/UserCreationTests.cs
@@ -5,108 +5,22 @@ using System.Text;
 using System.Threading.Tasks;
 
 using NUnit.Framework;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome; // chrome use
-using OpenQA.Selenium.Support.UI;
 
 namespace WebAddressBookTests
 {
     [TestFixture]
-    class UserCreationTests
+    public class UserCreationTests : TestBase
     {
-        private IWebDriver driver;
-        private string baseURL;
-
-        [SetUp]
-        public void SetupTest()
-        {
-            driver = new ChromeDriver();
-            baseURL = "http://127.0.0.1";
-        }
-
-        [TearDown]
-        public void TearDownTest()
-        {
-            try
-            {
-                driver.Close();
-                driver.Quit();
-            }
-            catch (Exception)
-            {
-                //
-            }
-        }
-
         [Test]
         public void UserCreationTest()
         {
-            OpenHomePage();
-
-            Login(new AccountData("admin", "secret"));
-
-            GoToUserAddPage();
-
-            //FillUserForm();
-            UserData user = new UserData("antony");
+            UserData user = new UserData("antonyNEW");
             user.LastName = "wise";
             user.Address = "russia, spb";
             user.Telephone = "89005555555";
             user.Email = "test@mail.ru";
-            FillUserForm(user);
 
-            SubmitUserCreation();
-
-            ReturnHomePage();
-        }
-
-        private void ReturnHomePage()
-        {
-            driver.FindElement(By.LinkText("home")).Click();
-        }
-
-        private void SubmitUserCreation()
-        {
-            //driver.FindElement(By.XPath("(.//*[normalize-space(text()) and normalize-space(.)='Notes:'])[1]/following::input[1]")).Click();
-            driver.FindElement(By.Name("submit")).Click();
-        }
-
-        private void FillUserForm(UserData user)
-        {
-            driver.FindElement(By.Name("firstname")).Click();
-            driver.FindElement(By.Name("firstname")).Clear();
-            driver.FindElement(By.Name("firstname")).SendKeys(user.FirstName);
-            driver.FindElement(By.Name("lastname")).Click();
-            driver.FindElement(By.Name("lastname")).Clear();
-            driver.FindElement(By.Name("lastname")).SendKeys(user.LastName);
-            driver.FindElement(By.Name("address")).Click();
-            driver.FindElement(By.Name("address")).Clear();
-            driver.FindElement(By.Name("address")).SendKeys(user.Address);
-            driver.FindElement(By.Name("home")).Click();
-            driver.FindElement(By.Name("home")).Clear();
-            driver.FindElement(By.Name("home")).SendKeys(user.Telephone);
-            driver.FindElement(By.Name("email")).Click();
-            driver.FindElement(By.Name("email")).Clear();
-            driver.FindElement(By.Name("email")).SendKeys(user.Email);
-        }
-
-        private void GoToUserAddPage()
-        {
-            driver.FindElement(By.LinkText("add new")).Click();
-        }
-
-        private void Login(AccountData account)
-        {
-            driver.FindElement(By.Name("user")).Clear();
-            driver.FindElement(By.Name("user")).SendKeys(account.Username);
-            driver.FindElement(By.Name("pass")).Clear();
-            driver.FindElement(By.Name("pass")).SendKeys(account.Password);
-            driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
-        }
-
-        private void OpenHomePage()
-        {
-            driver.Navigate().GoToUrl(baseURL + "/addressbook/");
+            appManager.User.Create(user);
         }
 
     }
